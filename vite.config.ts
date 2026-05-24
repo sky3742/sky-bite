3a0130ec17164effed91d6a696ca165609f24576
Sky <skyblue3742@gmail.com>
2026-05-24 23:03:23 +0800

    fix: remove pnpm-workspace.yaml, fix chunk warning

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		chunkSizeWarningLimit: 1000
+	},
 	test: {
 		include: ['src/**/*.test.ts']
 	},
